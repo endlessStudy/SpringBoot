f1597fbf62f8fab745afcac3b867df929b4f3914
tearsmart <461274926@qq.com>
2019-05-31 20:46:47 +0800

    fasdf

---
+++ security/src/main/java/com/tearsmart/security/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.tearsmart.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * <p>
+ * |****************************** *_* ******************************|
+ * |   __                                                      __    |
+ * | _/  |_  ____ _____ _______    ______ _____ _____ ________/  |_  |
+ * | \   __\/ __ \\__  \\_  __ \  /  ___//     \\__  \\_  __ \   __\ |
+ * |  |  | \  ___/ / __ \|  | \/  \___ \|  Y Y  \/ __ \|  | \/|  |   |
+ * |  |__|  \___  >____  /__|    /____  >__|_|  (____  /__|   |__|   |
+ * |            \/     \/             \/      \/     \/              |
+ * |                                                                 |
+ * |****************************** *_* ******************************|
+ * </p>
+ * @author tear-smart
+ * @date 2019-05-31
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").hasAnyRole("admin")
+                .anyRequest()
+                .authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("123456")).roles("admin")
+                .and()
+                .withUser("liuyl").password(new BCryptPasswordEncoder().encode("123456")).roles("admin");
+    }
+}
---
+++ security/src/main/java/com/tearsmart/security/controller/SecurityController.java
@@ -0,0 +1,35 @@
+package com.tearsmart.security.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * <p>
+ * |****************************** *_* ******************************|
+ * |   __                                                      __    |
+ * | _/  |_  ____ _____ _______    ______ _____ _____ ________/  |_  |
+ * | \   __\/ __ \\__  \\_  __ \  /  ___//     \\__  \\_  __ \   __\ |
+ * |  |  | \  ___/ / __ \|  | \/  \___ \|  Y Y  \/ __ \|  | \/|  |   |
+ * |  |__|  \___  >____  /__|    /____  >__|_|  (____  /__|   |__|   |
+ * |            \/     \/             \/      \/     \/              |
+ * |                                                                 |
+ * |****************************** *_* ******************************|
+ * </p>
+ * @author tear-smart
+ * @date 2019-05-31
+ */
+@Controller
+public class SecurityController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello(){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return JSON.toJSONString(user);
+    }
+}
